4d74d4c0ac59eff25ac4bb61f4e3e47b0da4e19e
Pavel Strashnov <paul.engine@gmail.com>
2014-01-21 17:51:12 +0400

    UI libs updated to recent versions

--- web/public/js/controllers/header.js
+++ web/public/js/controllers/header.js
@@ -3,13 +3,16 @@
 angular.module('autostop.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
-    $scope.menu = [{
-        'title': 'Articles',
-        'link': 'articles'
-    }, {
-        'title': 'Create New Article',
-        'link': 'articles/create'
-    }];
-    
+    $scope.menu = [
+//        {
+//            'title': 'Articles',
+//            'link': 'articles'
+//        },
+//        {
+//            'title': 'Create New Article',
+//            'link': 'articles/create'
+//        }
+    ];
+
     $scope.isCollapsed = false;
 }]);
