86fa7e5020ca3f4a016949117d9e371934e9205a
Pavel Strashnov <paul.engine@gmail.com>
2014-02-13 14:41:30 +0400

    Added server-side validation support for signup form

--- public/js/controllers/signup.js
+++ public/js/controllers/signup.js
@@ -1,11 +1,6 @@
 'use strict';
 
 angular.module('autostop.users').controller('SignupController', [ '$scope', '$log', '$modal', 'Users', function ($scope, $log, $modal, Users) {
-//    $scope.name = 'Ivan Ivanov';
-//    $scope.email = 'ivan.ivanov@mail.com';
-//    $scope.phone = '+7 123 456 7890'
-//    $scope.password = '1234';
-
     $scope.open = function () {
         $modal.open({
             templateUrl: 'views/users/signup.html',
@@ -13,10 +8,10 @@ angular.module('autostop.users').controller('SignupController', [ '$scope', '$lo
             resolve: {
                 defaults: function () {
                     return {
-                        name: $scope.name,
-                        email: $scope.email,
-                        phone: $scope.phone,
-                        password: $scope.password
+                        name: 'Ivan Ivanov',
+                        email: 'ivan.ivanov@mail.com',
+                        phone: '+7 123 456 7890',
+                        password: '1234'
                     };
                 }
             }
@@ -34,7 +29,7 @@ angular.module('autostop.users').controller('SignupController', [ '$scope', '$lo
             $modalInstance.dismiss('cancel');
         };
 
-        $scope.signup = function () {
+        $scope.signup = function (form) {
             var user = new Users($scope.user);
             user.username = user.email;
 
@@ -42,8 +37,18 @@ angular.module('autostop.users').controller('SignupController', [ '$scope', '$lo
             $log.info(user);
 
             user.$save(function () {
-                $log.info(' User created');
-            });
+                    $log.info(' User created');
+                },
+                function (err) {
+                    $log.warn(err.data);
+                    form.email.$setViewValue(form.name.$viewValue);
+                    form.email.$setValidity('unique', false);
+                });
+        };
+
+        //TODO: move to directive
+        $scope.resetUnique = function (email) {
+            email.$setValidity('unique', true);
         };
     }
 }]);
