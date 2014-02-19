312586e48f5776a457f7793d8d398cd4a0af843d
Pavel Strashnov <paul.engine@gmail.com>
2014-02-19 16:01:00 +0400

    Implemented trip editing

--- public/js/controllers/trips.js
+++ public/js/controllers/trips.js
@@ -5,7 +5,19 @@ angular.module('autostop.trips').controller('TripsController', [ '$scope', '$roo
         var now = new Date();
         return new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, 0);
     }
-    
+
+    function today(date) {
+        var now = new Date();
+        date = date || getNextHour();
+        return new Date(now.getFullYear(), now.getMonth(), now.getDate(), date.getHours(), date.getMinutes());
+    };
+
+    function tomorrow(date) {
+        var now = new Date();
+        date = date || getNextHour();
+        return new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, date.getHours(), date.getMinutes());
+    };
+
     $scope.trips = Trips.query();
 
     $rootScope.$on('trip-create-request', function (ev, data) {
@@ -29,6 +41,23 @@ angular.module('autostop.trips').controller('TripsController', [ '$scope', '$roo
         });
     };
 
+    $scope.edit = function (trip) {
+        var modal = $modal.open({
+            templateUrl: 'views/trips/item.html',
+            controller: EditTripModalController,
+            resolve: {
+                trip: function () {
+                    return trip;
+                }
+            }
+        });
+
+        modal.result.then(function (updatedTrip) {
+            var index = $scope.trips.indexOf(trip);
+            $scope.trips.splice(index, 1, updatedTrip);
+        });
+    };
+
     function CreateTripModalController($scope, $modalInstance, route) {
         $scope.route = route;
         $scope.trip = {
@@ -39,20 +68,48 @@ angular.module('autostop.trips').controller('TripsController', [ '$scope', '$roo
         };
 
         $scope.today = function () {
-            var start = $scope.trip.start || getNextHour();
-            var now = new Date();
-            $scope.trip.start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), start.getHours(), start.getMinutes());
+            $scope.trip.start = today($scope.trip.start);
         };
 
         $scope.tomorrow = function () {
-            var start = $scope.trip.start || getNextHour();
-            var now = new Date();
-            $scope.trip.start = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, start.getHours(), start.getMinutes());
+            $scope.trip.start = tomorrow($scope.trip.start);
         };
 
         $scope.ok = function () {
             var trip = new Trips($scope.trip);
             trip.$save(function (trip) {
+                $modalInstance.close(trip);
+            });
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }
+
+    function EditTripModalController($scope, $modalInstance, trip) {
+        $scope.trip = _.extend(trip, {
+            start: new Date(trip.start)
+        });
+
+        $scope.route = trip.route;
+
+        $scope.today = function () {
+            $scope.trip.start = today($scope.trip.start);
+        };
+
+        $scope.tomorrow = function () {
+            $scope.trip.start = tomorrow($scope.trip.start);
+        };
+
+        $scope.ok = function () {
+            var trip = new Trips({
+                start: $scope.trip.start,
+                seats: $scope.trip.seats,
+                note: $scope.trip.note
+            });
+
+            trip.$update({ tripId: $scope.trip._id }, function (trip) {
                 $modalInstance.close(trip);
             });
         };
