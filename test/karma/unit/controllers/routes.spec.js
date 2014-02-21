d194f164eb0c24019dd3ed3378178d7a678711ac
Pavel Strashnov <paul.engine@gmail.com>
2014-02-21 12:30:27 +0400

    Added unit tests for RoutesController

--- test/karma/unit/controllers/routes.spec.js
+++ test/karma/unit/controllers/routes.spec.js
@@ -1,40 +1,117 @@
 'use strict';
 
 describe('RoutesController', function () {
-    var scope,
-        RoutesController;
+    var $controller,
+        $rootScope,
+        $httpBackend,
+        $q;
 
     beforeEach(module('autostop.routes'));
 
-    beforeEach(inject(function ($controller, $rootScope) {
-        scope = $rootScope.$new();
-        RoutesController = $controller('RoutesController', {
-            $scope: scope,
-            Global: { user: { _id: '1', name: 'Pavel'}}
-        });
+    beforeEach(inject(function (_$controller_, _$rootScope_, _$httpBackend_, _$q_) {
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+        $httpBackend = _$httpBackend_;
+        $q = _$q_;
     }));
 
     it('should be defined', function () {
+        //Arrange
+        var $scope = $rootScope.$new(),
+            RoutesController = $controller('RoutesController', {
+                $scope: $scope,
+                Global: { user: { _id: '1', name: 'Pavel'}}
+            });
+
+        //Assert
         expect(RoutesController).toBeDefined();
     });
 
     describe('routes list', function () {
-        var $httpBackend;
+        var $scope,
+            RoutesController;
 
-        beforeEach(inject(function (_$httpBackend_) {
-            $httpBackend = _$httpBackend_;
-            $httpBackend.whenGET('/api/routes?creator=1')
-                .respond([{ _id: '1', title: 'Route title' }]);
-        }));
+        beforeEach(function () {
+            $scope = $rootScope.$new();
 
-        it('should populate routes on load for current user', function () {
-            $httpBackend.flush();
-            expect(scope.routes.length).toEqual(1);
+            RoutesController = $controller('RoutesController', {
+                $scope: $scope,
+                Global: { user: { _id: '1', name: 'Pavel'}}
+            });
         });
 
-        afterEach(function() {
+        it('should be populated on load for current user', function () {
+            //Arrange
+            $httpBackend.whenGET('/api/routes?creator=1')
+                .respond([
+                    { _id: '1', title: 'Route title' }
+                ]);
+
+            //Act
+            $httpBackend.flush();
+
+            //Assert
+            expect($scope.routes.length).toEqual(1);
+        });
+
+        afterEach(function () {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
+        });
+    });
+
+    describe('create new route dialog', function () {
+        var $modalMock,
+            $scope,
+            defer;
+
+        beforeEach(inject(function ($q) {
+            defer = $q.defer();
+            $modalMock = jasmine.createSpyObj('$modal', ['open']);
+            $modalMock.open.andReturn({
+                result: defer.promise
+            });
+            $scope = $rootScope.$new();
+        }));
+
+        it('should be able to open', function () {
+            //Arrange
+            $controller('RoutesController', {
+                $scope: $scope,
+                $modal: $modalMock,
+                Global: { user: { _id: '1', name: 'Pavel'}}
+            });
+
+            //Act
+            $scope.add();
+
+            //Assert
+            expect($modalMock.open).toHaveBeenCalled();
+        });
+
+        it('should pass created route back to scope', function () {
+            //Arrange
+            $controller('RoutesController', {
+                $scope: $scope,
+                $modal: $modalMock,
+                Global: { user: { _id: '1', name: 'Pavel'}}
+            });
+            var route = {
+                _id: 1,
+                title: 'Route title'
+            };
+            $httpBackend.whenGET('/api/routes?creator=1')
+                .respond([]);
+
+            //Act
+            $httpBackend.flush();
+            $scope.add();
+            defer.resolve(route);
+            $scope.$digest();
+
+            //Assert
+            expect($scope.routes.length).toEqual(1);
+            expect($scope.routes[0]).toBe(route);
         });
     });
 });
